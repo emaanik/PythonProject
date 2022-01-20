ebf8e9f5704535660b21196d7e6850bc12591799
emaanik <97445694+emaanik@users.noreply.github.com>
2022-01-20 13:02:09 +0000

    Add files via upload
    I have started on the python assignment. I have decided I am going to make a higher or lower card game. The user will get 3 live which will be represented on the LEDS.

---
+++ PythonAssignment.py
@@ -0,0 +1,13 @@
+f= open ('higherorlowergame.txt','r')
+
+print(''.join([line for line in f]))
+
+#from random import randint
+
+#number = randint(1,100)
+#score = 0
+
+#print("Current Number: " + str(number))
+
+#userGuess = input("Will the next number be H or L?")
+
