64088723dc4ee8953fa1a685d635a640d3c73078
emaanik <97445694+emaanik@users.noreply.github.com>
2022-01-22 13:28:58 +0000

    Update PythonAssignment.py
    Used unicode to make the suits, and made the ranks. Concatenated them to avoid making individual variables for each.

--- PythonAssignment.py
+++ PythonAssignment.py
@@ -1,13 +1,10 @@
+import random
 f= open ('higherorlowergame.txt','r')
 
 print(''.join([line for line in f]))
 
-#from random import randint
+deck=[]
 
-#number = randint(1,100)
-#score = 0
-
-#print("Current Number: " + str(number))
-
-#userGuess = input("Will the next number be H or L?")
+ for suit in = ['\u2663', '\u2660' , '\u2665', '\u2666']:
+    for ranks in ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 
