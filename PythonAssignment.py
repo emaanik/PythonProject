0db16db45f9a488d85e2501ad8aea6b13a6330fe
emaanik <97445694+emaanik@users.noreply.github.com>
2022-01-22 13:38:43 +0000

    Update PythonAssignment.py

--- PythonAssignment.py
+++ PythonAssignment.py
@@ -6,5 +6,6 @@ print(''.join([line for line in f]))
 deck=[]
 
  for suit in = ['\u2663', '\u2660' , '\u2665', '\u2666']:
-    for ranks in ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-
+    for ranks in ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]:
+    deck.append(ranks+suit)
+    
