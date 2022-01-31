31ff471982ddfef8cff607dc0e36972581046f1c
emaanik <97445694+emaanik@users.noreply.github.com>
2022-01-31 11:13:25 +0000

    Update PythonAssignment.py
    I made the game so that there is a limited number of lives. I will be putting the lives on the OPC

--- PythonAssignment.py
+++ PythonAssignment.py
@@ -1,11 +1,75 @@
+import opc
 import random
+import colorsys
+
 f= open ('higherorlowergame.txt','r')
 
-print(''.join([line for line in f]))
+print(''.join([line for line in f])) 
 
-deck=[]
+ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"] #
+suits = ['\u2663', '\u2660' , '\u2665', '\u2666']
+deck = []
 
-for suit in ['\u2663', '\u2660' , '\u2665', '\u2666']:
-    for ranks in ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]:
-     deck.append(ranks+suit)
+leds = [(255,255,255)]*360 #white leds
+
+#3,4,5,6,62,67, 121, 128, 181,188, 242, 247,303,304,305 list of leds for 1 circle
+ 
+
+
+value = 1
+for rank in ranks:
+    for suit in suits:
+        deck.append([rank + suit, value])#combines rank and suit
+    value = value + 1
+
+random.shuffle(deck)
+no_of_lives = 3
+card1 = deck.pop(0)
+
+while True:
+    
+    print("Your number of lives is", no_of_lives)
+    print("\n\nThe current card is", card1[0])
+    while True:
+        choice = input("higher or lower?")
+        if len(choice) > 0:
+            if choice[0].lower() in ["h","l"]:
+                break
+        
+
+    card2 = deck.pop(0)
+    print("The next card picked is", card2[0])
+    time.sleep(1)
+
+    if choice[0].lower() == "h" and card2[1] > card1[1]:
+        print("Correct!")
+        no_of_lives +=0
+        time.sleep(1)
+    if choice[0].lower() == "h" and card2[1] < card1[1]:
+        print("Wrong!")
+        time.sleep(1)
+        no_of_lives -=1
+        
+    if choice[0].lower() == "l" and card2[1] < card1[1]:
+        print("Correct!")
+        no_of_lives +=0
+        time.sleep(1)
+    if choice[0].lower() == "l" and card2[1] > card1[1]:
+        print("Wrong!")
+        time.sleep(1)
+        no_of_lives -=1
+
+    if no_of_lives <= 0:
+        print("you lose!")
+    else:
+        print("draw!")
+
+    card1 = card2
+
+
+print("Game over!")
+
+time.sleep(4)
+
+
     
